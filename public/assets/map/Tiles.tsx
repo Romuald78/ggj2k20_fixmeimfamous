f6c20a4ce659ce29c951595be1b78ac2e455356d
Pierre-Henri <pierreh37@gmail.com>
2020-02-01 01:53:58 +0100

    add map background

---
+++ public/assets/map/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Tiles" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="Tiles.png" width="192" height="192"/>
+</tileset>
